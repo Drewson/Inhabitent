553cb4e07233eae375cbf1b702069750aeed542d
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-02-20 07:15:12 -0800

    Changed the menu, and some styles

--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -9,13 +9,13 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			<header class="about-head">
+			</header>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
