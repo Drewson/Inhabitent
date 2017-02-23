9c77486eca5b22519dadb55e7f5c52b8fb19e22d
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-02-23 10:07:40 -0800

    Changed button to a tag

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -16,16 +16,16 @@ get_header('home'); ?>
     <?php    
         $terms = get_terms( array(
           'taxonomy' => 'product_type',
-           'orderby' => 'name', ));
+           ));
         foreach ($terms as $term) :
-          $url = get_term_link ($term->slug , 'product_type');              
+          $url = get_term_link( $term->slug , 'product_type');              
     ?>  
       <div class="shop-stuff-item">
-          <div class="product-icon-image">       
+          <div>       
               <img src="<?php echo get_template_directory_uri();?>/images/product-type-icons/<?php echo $term->slug; ?>.svg" alt="">
           </div>
           <p><?php echo $term->description; ?></p>
-          <button class="categ-button"><a href='<?php echo $url?>' class='button-link'><?php echo $term->name; ?></a></button> 
+          <a href='<?php echo $url?>' class='button-link'><?php echo $term->name; ?></a>
 	</div>
                         
     <?php endforeach; ?>
