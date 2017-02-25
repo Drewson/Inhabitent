c92043baa34a3767e3adfa12c556f6fb620ff589
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-02-25 15:44:56 -0800

    made the footer truly widgitized

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -9,15 +9,18 @@
 
 			</div><!-- #content -->
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				
+				<div class='main-footer-container'>
+					<div class='footer-widgets'>
+						<?php dynamic_sidebar( 'footer' ); ?>
+					</div>
 					<div class="inhab-logo-text">
 						<img src ="<?php echo get_template_directory_uri();?>/images/logos/inhabitent-logo-text.svg" />
 					</div>
+				</div>
 				</div><!-- .site-info -->
 				<div class="copy-right"> 
 					</p>COPYRIGHT © 2016 INHABITENT</p>
 				</div>
-				<?php wp_footer(); ?>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
